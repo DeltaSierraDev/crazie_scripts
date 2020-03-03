ef8235c53cb114b09703c8a0ca884c036e024178
crazieNephilim <37982778+crazieNephilim@users.noreply.github.com>
2020-03-03 22:44:08 +0100

    Create fresh_install.sh

---
+++ fresh_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install default-jdk -y
+sudo apt-get install git-all -y
+sudo apt-get install python3 -y
+sudo apt-get install gnome-tweaks
+sudo apt-get install inkscape -y 
+sudo apt-get install gimp -y 
+sudo apt-get install blender -y 
+sudo apt-get install audacity -y 
+sudo apt-get install lmms -y 
+sudo apt-get install rawtherapee -y 
+sudo apt-get install vlc -y 
+sudo apt-get install clamav -y 
+sudo snap install gitkraken
+sudo snap install code
+sudo apt-get update -y
+sudo apt-get upgrade -y
+notify-send 'Fresh install script' 'Successfully finished'
